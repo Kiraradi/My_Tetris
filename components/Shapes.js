169e02780c1c1da43c88d1c77dd0a8437a5cfa86
Kiraradi <katel1998@yandex.ru>
2024-09-13 12:35:06 +0300

    added the creation of a shape and moving it to the field

--- components/Shapes.js
+++ components/Shapes.js
@@ -1,6 +1,76 @@
 const Shapes = class {
-    constructor () {
-        this.types = ['i', 'z', 'o', 'l']
-        this
+    constructor (startPosition) {        
+        this.currentType = this.#setRandomType();
+        this.startPosition = startPosition;
+        this.shape = null;
+        this.createShape();        
     }
+
+    #setRandomType() {
+        //['i', 'z', 'o', 'l', 's', 't']
+        const types = ['o']
+        return types[Math.floor(Math.random() * types.length)];
+    }
+
+    createShape() {
+         if(this.currentType === 'o') {
+            this.shape = this.createShapeTypeO();
+         }
+    }
+
+    createShapeTypeO() {
+        const bodyShapeForTypeO = Array.from({length: 2},() => Array.from({length:2}, () => {
+            return {
+                pixelPosition: {x:0, y:0},
+                pixelContent: 1 
+            }
+        }));
+        
+        for(let column = 0; column < bodyShapeForTypeO.length; column++) {
+            for(let row = 0; row < bodyShapeForTypeO[column].length; row++) {
+                bodyShapeForTypeO[column][row].pixelPosition.x = this.startPosition.x - (bodyShapeForTypeO.length ) + column;
+                bodyShapeForTypeO[column][row].pixelPosition.y = this.startPosition.y + row;
+            }
+        }
+
+        return bodyShapeForTypeO
+
+    }
+
+    stepDown(lastPosition) {
+        const bottomPixelPosition = this.shape[this.shape.length - 1][0].pixelPosition.x;
+
+        this.shape.forEach(column => {
+            if (bottomPixelPosition < lastPosition) {
+                column.forEach(pixel => pixel.pixelPosition.x = ++pixel.pixelPosition.x)
+            }
+            
+        })
+    }
+
+    stepLeft() {
+        const leftPixelPosition = this.shape[0][0].pixelPosition.y;
+
+        this.shape.forEach(column => {
+            if (leftPixelPosition > 0) {
+                column.forEach(pixel => pixel.pixelPosition.y = --pixel.pixelPosition.y)
+            }            
+        })
+    }
+
+
+    stepRight(lastPosition) {
+        const rightPixelPosition = this.shape[this.shape.length - 1][this.shape[this.shape.length - 1].length - 1].pixelPosition.y;
+        this.shape.forEach(column => {
+            
+            if (rightPixelPosition < lastPosition) {
+                column.forEach(pixel => pixel.pixelPosition.y = ++pixel.pixelPosition.y)
+            }            
+        })
+
+    }
+
+
 }
+
+export default Shapes;
--- components/Tetris.js
+++ components/Tetris.js
@@ -1,42 +1,105 @@
 import { createElementByTag } from "../services/serveces.js";
+import Shapes from "./Shapes.js";
 
 const Tetris = class {
     constructor() {
-        this.field = this.initField();
+        this.field = null;
         this.score = 0;
+        this.currentShape = null;
+        this.tetrisFieldElem = null
     }
 
     drowUI(wrapperComponent) {
         const tetrisElem = createElementByTag('div', 'tetris');        
-        const tetrisfieldElem = createElementByTag('div', 'tetrisfield');
+        this.tetrisFieldElem = createElementByTag('div', 'tetrisfield');
 
         this.scoreElem =  createElementByTag('h2', 'score', `Ваш счет: ${this.score}`);
-        tetrisElem.append(this.scoreElem);
-        
+        tetrisElem.append(this.scoreElem);        
 
-        tetrisElem.append(tetrisfieldElem);
-        this.drowField(tetrisfieldElem);
+        tetrisElem.append(this.tetrisFieldElem);
+        this.initField();
+        this.drowField();
 
         wrapperComponent.append(tetrisElem);
+
+        this.subscribeToEvents()
     }
 
-    drowField(container) {
-        this.field.map(column => {
+    drowField() {
+        this.tetrisFieldElem.innerHTML = '';
+
+        this.field.forEach(column => {
             const rowElem = createElementByTag('div', 'row');
             column.forEach(cell => {
                 const className = cell ? 'cell full_cell': `cell`
                 const cellElem = createElementByTag('div', className);
                 rowElem.append(cellElem);
             })
-            container.append(rowElem);
+
+            this.tetrisFieldElem.append(rowElem);
             
         })
     } 
 
     initField() {
-        const field = Array.from({length: 20},() => Array.from({length:10}, () => 0));
+        this.field = Array.from({length: 20},() => Array.from({length:10}, () => 0));
+        this.startPositionForNewShapes = {x:0, y: Math.floor(this.field[0].length / 2) - 1};
+        this.currentShape = new Shapes(this.startPositionForNewShapes);
+        this.reRender()
+    }
 
-        return field 
+    reRender() {
+        this.currentShape.shape.flat().forEach(pixel => {
+            if (pixel.pixelPosition.x >= 0 && pixel.pixelPosition.y >= 0) {
+                this.field[pixel.pixelPosition.x][pixel.pixelPosition.y] = pixel.pixelContent;
+            }
+        });      
+
+        this.drowField();
+    }
+
+    deleteLastPositionShape() {
+        this.currentShape.shape.flat().forEach(pixel => {
+            if (pixel.pixelPosition.x >= 0 && pixel.pixelPosition.y >= 0) {
+                this.field[pixel.pixelPosition.x][pixel.pixelPosition.y] = 0;
+            }
+        });
+    }
+
+
+    onStepDown() {
+        this.deleteLastPositionShape();
+        this.currentShape.stepDown(this.field.length - 1);
+        this.reRender();
+    }
+
+    onStepLeft() {
+        this.deleteLastPositionShape();
+        this.currentShape.stepLeft();
+        this.reRender();
+    }
+
+    onStepRight() {
+        this.deleteLastPositionShape();
+        this.currentShape.stepRight(this.field[0].length - 1);
+        this.reRender();
+    }
+
+    subscribeToEvents() {
+        document.addEventListener('keydown', (event) => {
+            event.preventDefault();
+            if (event.code === 'ArrowDown') {
+                this.onStepDown();
+            }
+
+            if (event.code === 'ArrowLeft') {
+                this.onStepLeft();
+            }
+
+            if (event.code === 'ArrowRight') {
+                this.onStepRight();
+            }            
+        })
     }
 }
 
